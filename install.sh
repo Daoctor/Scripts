e1331ff04d5dee24d3983211a17aacc418e28574
Daoctor <daoctor@gmail.com>
2020-04-20 00:23:08 +0800

    init vps

---
+++ install.sh
@@ -0,0 +1,54 @@
+# centos
+# install docker
+# install pyenv pyenv-virtualenv
+# install tmux
+# setup tmux config file
+
+yum update -y 
+
+# install docker 
+
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+
+yum install docker-ce docker-ce-cli containerd.io
+
+# start Docker
+
+systemctl start docker
+
+
+yum install -y git 
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
+# install python3
+
+pyenv install 3.6.5
+
+yum install -y tmux
+
+
+
+
+
+
+
