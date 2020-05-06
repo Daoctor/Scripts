df75dd6f203b163ae3ff08ed8e9e903d02e6702d
Daoctor <daoctor@gmail.com>
2020-05-06 08:15:35 +0800

    set timezone

--- install.sh
+++ install.sh
@@ -35,7 +35,13 @@ curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compo
 
 chmod +x /usr/local/bin/docker-compose
 
-yum install -y git 
+# set timezone to Asia/Shanghai
+
+timedatectl set-timezone Asia/Shanghai
+
+
+# install pyenv 
+yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel vim tmux git
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
@@ -43,14 +49,11 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
 
 source /root/.bash_profile
-
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel vim
 
 # install python3
 pyenv install 3.6.5
 
-yum install -y tmux
 
 wget https://raw.githubusercontent.com/Daoctor/Scripts/master/tmux.conf -O ~/.tmux.conf
 
@@ -58,8 +61,4 @@ wget https://raw.githubusercontent.com/Daoctor/Scripts/master/tmux.conf -O ~/.tm
 sudo grep -q "ChallengeResponseAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*ChallengeResponseAuthentication[[:space:]]yes.*/c\ChallengeResponseAuthentication no" /etc/ssh/sshd_config || echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
 sudo grep -q "^[^#]*PasswordAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no" /etc/ssh/sshd_config || echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 sudo service sshd restart
-
-
-
-
 
